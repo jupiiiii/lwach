5bd628043ead9e44148ff9255c4093c85e133956
jupiiiii <98879512+jupiiiii@users.noreply.github.com>
2024-08-15 15:37:36 +0200

    Update lwach.js

--- lwach.js
+++ lwach.js
@@ -1,8 +1,10 @@
 const signupForm = document.getElementById('signup_form');
+const loginForm = document.getElementById('login_form');
 
 signupForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     try {
@@ -11,7 +13,8 @@ signupForm.addEventListener('submit', async (e) => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ username, password }),
+            body: JSON.stringify({ username, email, password }),
+
         });
 
         const result = await response.json();
@@ -23,5 +26,33 @@ signupForm.addEventListener('submit', async (e) => {
         }
     } catch (error) {
         alert('Error during sign up: ' + error.message);
+    }
+});
+
+loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    // const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('https://lwach-7ba065977414.herokuapp.com/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            alert(result.message);
+            // Optionally, redirect to another page or perform other actions
+        } else {
+            alert(result.message);
+        }
+    } catch (error) {
+        alert('Error during login: ' + error.message);
     }
 });
